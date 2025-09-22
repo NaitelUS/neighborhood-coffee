9f273ae1d2544089866b7984ab7e4e02bb314ab9
NaitelUS <97500278+NaitelUS@users.noreply.github.com>
2025-09-21 23:33:14 -0600

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,25 +4,19 @@ import OrderPage from "@/pages/OrderPage";
 import ThankYou from "@/pages/ThankYou";
 import OrderStatus from "@/pages/OrderStatus";
 import AdminOrders from "@/pages/AdminOrders";
-import Header from "@/components/Header";
+import Layout from "@/components/Layout";
 
 export default function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col bg-gray-50">
-        {/* Header con logo + frase */}
-        <Header />
-
-        {/* Contenido de páginas */}
-        <main className="flex-1 container mx-auto px-4 py-6">
-          <Routes>
-            <Route path="/" element={<OrderPage />} />
-            <Route path="/thank-you" element={<ThankYou />} />
-            <Route path="/order-status/:id" element={<OrderStatus />} />
-            <Route path="/admin/orders" element={<AdminOrders />} />
-          </Routes>
-        </main>
-      </div>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<OrderPage />} />
+          <Route path="/thank-you" element={<ThankYou />} />
+          <Route path="/order-status/:id" element={<OrderStatus />} />
+          <Route path="/admin/orders" element={<AdminOrders />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
