bd018574ddac31ca9cea491173ff38cdaba0c7e0
NaitelUS <97500278+NaitelUS@users.noreply.github.com>
2025-09-26 07:44:17 -0600

    Create airtableClient.ts

---
+++ netlify/lib/airtableClient.ts
@@ -0,0 +1,10 @@
+import Airtable from "airtable";
+
+if (!process.env.AIRTABLE_API_KEY || !process.env.AIRTABLE_BASE_ID) {
+  throw new Error("Missing Airtable environment variables");
+}
+
+// Instancia única de Airtable
+export const base = new Airtable({
+  apiKey: process.env.AIRTABLE_API_KEY,
+}).base(process.env.AIRTABLE_BASE_ID);
