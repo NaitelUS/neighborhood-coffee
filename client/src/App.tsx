2baddfe1a0c919941e26359cbc30fdd42718f172
NaitelUS <97500278+NaitelUS@users.noreply.github.com>
2025-10-23 13:19:09 -0600

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import "./App.css";
 // 🧩 Componentes principales
 import Header from "./components/Header";
 import Menu from "./components/Menu";
